465cbe305b2bf68df0da9d11687bd8a71c7ceadd
Paul Stalenhoef <paul.stalenhoef@gmail.com>
2022-09-17 22:17:38 +0200

    Updated solution using the coerceIn function

--- src/main/kotlin/nl/stalenhoef/codewars/conversion/RgbToHexConversion.kt
+++ src/main/kotlin/nl/stalenhoef/codewars/conversion/RgbToHexConversion.kt
@@ -2,10 +2,4 @@ package nl.stalenhoef.codewars.conversion
 
 fun rgb(r: Int, g: Int, b: Int) = "${r.asHex()}${g.asHex()}${b.asHex()}"
 
-fun Int.asHex() = "%02X".format(this.roundInt())
-
-fun Int.roundInt() = when {
-    (this < 0) -> 0
-    (this > 255) -> 255
-    else -> this
-}
+fun Int.asHex() = "%02X".format(this.coerceIn(0, 255))
